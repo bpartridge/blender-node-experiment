9eea2833cd9e4f9b2bf25f415f705bf14bc0c3bc
Brenton Partridge <bapartridge@gmail.com>
2013-03-11 15:54:55 -0400

    Possible fix to template

--- template.py
+++ template.py
@@ -1,6 +1,8 @@
 
+import bpy
+
 try:
   bpy.data.images['strapimage'].filepath = "//{{strapimage}}"
   bpy.data.images['bodyimage'].filepath = "//{{bodyimage}}"
 except Exception as e:
-  print "Error:", e.strerror
+  print(e)
