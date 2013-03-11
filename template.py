60eb8bd8c6b3a8c36e6a74bc1a2fda5bd3a08ddf
Brenton Partridge <bapartridge@gmail.com>
2013-03-11 19:55:40 -0400

    Allow stretching of strap

--- template.py
+++ template.py
@@ -1,7 +1,15 @@
 
 import bpy
 
+def z_stretch_obj(obj, factor):
+  vs = obj.data.vertices
+  minz = min([v.co.z for v in vs])
+  for v in vs:
+    dz = v.co.z - minz
+    v.co.z = dz * factor + minz
+
 try:
+  z_stretch_obj(bpy.data.objects['strap'], float("{{straplen}}" or 1))
   bpy.data.images['strapimage'].filepath = "//{{strapimage}}"
   bpy.data.images['bodyimage'].filepath = "//{{bodyimage}}"
 except Exception as e:
